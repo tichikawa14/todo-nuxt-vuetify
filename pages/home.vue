5da182a36718a00b6a944748863422c20b50088a
tichikawa14 <gakusei.umeeee@gmail.com>
2020-01-25 15:55:05 +0900

    initial commit

---
+++ pages/home.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-center>
+      <img src="/v.png" alt="Vuetify.js" class="mb-5">
+      <blockquote class="blockquote">
+        <h3>ホームです</h3>
+        <main-contents>
+          {{ todos[0].name }} / {{ todos[0].description }}
+        </main-contents>
+        <footer>
+          <small>
+            <em>&mdash;松田信介</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+  export default {
+    data: function () {
+      return {
+        todos: [
+          {
+            name: '001',
+            description: 'dfaknjbtra'
+          },
+          {
+            name: '002',
+            description: '13fgreflgf'
+          }
+        ],
+        todoItemObject: {
+          name: '',
+          description: ''
+        }
+      }
+    }
+  }
+</script>
