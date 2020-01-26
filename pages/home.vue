f9d399938bf7ec1d3d7dea3e017f94631b1812b2
tichikawa14 <gakusei.umeeee@gmail.com>
2020-01-26 11:53:40 +0900

    todoリストherokuから取得

--- pages/home.vue
+++ pages/home.vue
@@ -3,10 +3,18 @@
     <v-flex text-xs-center>
       <img src="/v.png" alt="Vuetify.js" class="mb-5">
       <blockquote class="blockquote">
-        <h3>ホームです</h3>
-        <main-contents>
-          {{ todos[0].name }} / {{ todos[0].description }}
-        </main-contents>
+        <h3>TODOを追加しましょう</h3>
+        <v-text-field v-model="todoItemObject.title" type="text" placeholder="タイトルを入力してください"/>
+        <v-text-field v-model="todoItemObject.description" type="text" placeholder="内容を入力してください"/>
+        <v-btn large color="red" v-on:click="addTodo">追加する</v-btn>
+        <div>
+          <h3>TODO一覧</h3>
+          <ul>
+            <li v-for="todo in todos" v-bind:key="todo.name" v-on:click="deleteTodo(todo.id)">
+              {{ todo.title }} / {{ todo.description }}
+            </li>
+          </ul>
+        </div>
         <footer>
           <small>
             <em>&mdash;松田信介</em>
@@ -16,25 +24,83 @@
     </v-flex>
   </v-layout>
 </template>
+​
 <script>
-  export default {
-    data: function () {
-      return {
-        todos: [
-          {
-            name: '001',
-            description: 'dfaknjbtra'
-          },
-          {
-            name: '002',
-            description: '13fgreflgf'
+import axios from 'axios'
+
+export default {
+  data: function () {
+    return {
+      todos: [],
+      todoItemObject: {
+        id: '',
+        title: '',
+        description: ''
+      }
+    }
+  },
+  created () {
+    this.getTodos()
+  },
+  methods: {
+    deleteTodo: function (id) {
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    },
+    async getTodos () {
+      try {
+        var result = await axios({
+          method: 'POST',
+          url: 'https://limitless-bayou-12897.herokuapp.com/graphql',
+          data: {
+            query:
+              `
+              query getToDos {
+                toDos {
+                  id
+                  title
+                  description
+                }
+              }
+            `
           }
-        ],
-        todoItemObject: {
-          name: '',
-          description: ''
-        }
+        })
+        console.log(result.data.data)
+        this.todos = result.data.data.toDos
+      } catch (error) {
+        // console.error(error);
+      }
+    },
+    async addTodo () {
+      try {
+        await axios({
+          method: 'POST',
+          url: 'https://limitless-bayou-12897.herokuapp.com/graphql',
+          data: {
+            query:
+              `
+              mutation {
+                createToDo (input:{
+                  data: {
+                    title: "${this.todoItemObject.title}",
+                    description: "${this.todoItemObject.description}",
+                    userName: "matsuda"
+                  }
+                }) {
+                  toDo {
+                    title
+                    description
+                    userName
+                  }
+                }
+              }
+            `
+          }
+        })
+        this.getTodos()
+      } catch (error) {
+        // console.error(error);
       }
     }
   }
+}
 </script>
